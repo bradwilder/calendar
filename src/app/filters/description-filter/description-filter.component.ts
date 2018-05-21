ebfbf23c315fd215b99f474f7946bfb7bdceb193
Brad <bradwilder31415@gmail.com>
2018-05-21 11:30:43 -0600

    Description filter

--- src/app/filters/description-filter/description-filter.component.ts
+++ src/app/filters/description-filter/description-filter.component.ts
@@ -1,14 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { FiltersService } from '../filters.service';
+import { CalendarService } from '../../calendar/calendar.service';
+import { Event } from '../../shared/event.model';
 
-@Component({
-  selector: 'app-description-filter',
-  templateUrl: './description-filter.component.html'
+@Component
+({
+	selector: 'app-description-filter',
+	templateUrl: './description-filter.component.html'
 })
-export class DescriptionFilterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class DescriptionFilterComponent implements OnInit, OnDestroy
+{
+	private static filterName = 'desc';
+	selectedValue = '';
+	enabled = false;
+	filtersEnabled = false;
+	enabledSubscription: Subscription;
+	clearedSubscription: Subscription;
+	inputTimeout: number;
+	
+	constructor(private filtersService: FiltersService, private calendarService: CalendarService) {}
+	
+	ngOnInit()
+	{
+		this.enabledSubscription = this.filtersService.enabledChanged.subscribe((enabled) =>
+		{
+			this.filtersEnabled = enabled;
+		});
+		
+		this.clearedSubscription = this.filtersService.cleared.subscribe(() =>
+		{
+			this.onClear();
+			this.enabled = false;
+			this.onEnable();
+		});
+	}
+	
+	onInput()
+	{
+		this.clearTimeout();
+		
+		this.inputTimeout = window.setTimeout(() =>
+		{
+			this.doFilter();
+		}, 2000);
+	}
+	
+	doFilter()
+	{
+		if (this.selectedValue)
+		{
+			this.filtersService.addFilter(DescriptionFilterComponent.filterName, this.filterFunction.bind(this));
+		}
+		else
+		{
+			this.filtersService.removeFilter(DescriptionFilterComponent.filterName);
+		}
+	}
+	
+	onEnable()
+	{
+		if (this.enabled)
+		{
+			if (this.selectedValue)
+			{
+				this.filtersService.addFilter(DescriptionFilterComponent.filterName, this.filterFunction.bind(this));
+			}
+		}
+		else
+		{
+			this.filtersService.removeFilter(DescriptionFilterComponent.filterName);
+			this.clearTimeout();
+		}
+	}
+	
+	onClear()
+	{
+		this.clearTimeout();
+		this.selectedValue = '';
+		this.filtersService.removeFilter(DescriptionFilterComponent.filterName);
+	}
+	
+	filterFunction(event: Event)
+	{
+		return event.description && event.description.indexOf(this.selectedValue) !== -1;
+	}
+	
+	clearTimeout()
+	{
+		if (this.inputTimeout)
+		{
+			window.clearTimeout(this.inputTimeout);
+		}
+	}
+	
+	ngOnDestroy()
+	{
+		this.enabledSubscription.unsubscribe();
+		this.clearedSubscription.unsubscribe();
+		
+		this.clearTimeout();
+	}
 }
